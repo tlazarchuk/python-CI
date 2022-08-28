563204478d251ae1830197f1e29297dd43a79199
Taras Lazarchuk <taraslaz10@ukr.net>
2022-08-28 14:06:17 +0300

    Commit with tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,9 @@ from app import index
 
 def test_index():
     assert index() == "Hello, world!"
+
+def test_home():
+    assert home() == "Hello, home!"
+
+def test_profile():
+    assert profile() == "It's your profile!"
